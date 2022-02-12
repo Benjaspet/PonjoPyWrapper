5cdf9e6a4bef0f5db5fb827870edb649fc40d0c9
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2022-02-12 10:01:10 -0500

    licensing, removed old request methods

--- src/utils/StartupUtil.py
+++ src/utils/StartupUtil.py
@@ -1,4 +1,22 @@
-import requests
+"""
+Copyright 2022 Ben Petrillo. All rights reserved.
+
+Project licensed under the MIT License: https://www.mit.edu/~amini/LICENSE.md
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+
+All portions of this software are available for public use, provided that
+credit is given to the original author(s).
+"""
+
+from src.http.RequestFactory import RequestFactory
 
 class StartupUtil:
 
@@ -6,7 +24,11 @@ class StartupUtil:
         self.key = key
 
     def isApiKeyValid(self) -> bool:
-        result = requests.get("https://app.ponjo.club/v1/covid", headers={"Authorization": self.key})
-        if result.status_code == 403:
+        response = RequestFactory(self.key)\
+            .setMethod("GET")\
+            .setURL("https://app.ponjo.club/v1/covid")\
+            .setHeaders({"Authorization": self.key})\
+            .build()
+        if response.status_code == 403:
             return False
         return True
